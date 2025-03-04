9fd2fbbdd0670cfe327bfae7f5d864d351d1636b
gulshairbutt <segullshairbutt@gmail.com>
2025-03-04 17:03:26 +0100

    feature/books-build
    Added the build script.

--- scripts/build_all_books.sh
+++ scripts/build_all_books.sh
@@ -3,6 +3,6 @@
 # docker build -t books-builder .
 for dir in books/*/; do
     if [ -d "$dir" ]; then
-        docker run --rm -v "$(pwd)/books/":/app/books books-builder jupyter-book build --path-output "/app/static/$(basename "$dir")" "/app/books/$(basename "$dir")"
+        docker run --rm -v "$(pwd)/books/":/app/books -v "$(pwd)/static/":/app/static books-builder jupyter-book build --path-output "/app/static/$(basename "$dir")" "/app/books/$(basename "$dir")"
     fi
 done
